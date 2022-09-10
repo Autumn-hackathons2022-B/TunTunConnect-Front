96452f0a4ceabb6e5f7d09d9b7ee50bf7fa12780
hirotask <hirotaskhashi@gmail.com>
2022-09-10 21:47:32 +0900

    [update]: つんつん通知を作った #5

--- src/components/atoms/TunTunIcon.js
+++ src/components/atoms/TunTunIcon.js
@@ -9,7 +9,7 @@ export const TunTunIcon = ({ size }) => {
       border: 1px solid #000000;
       filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
       border-radius: 151px;
-      margin-right: 10px;
+      margin-right: 11px;
     `;
 
     return <StyledAvatar
