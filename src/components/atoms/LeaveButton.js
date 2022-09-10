a824c48c3ebe8d84fcb982804fa85dab07d51407
MinamiInoue2323 <inoue@ics.keio.ac.jp>
2022-09-10 21:14:09 +0900

    [update] implement leavebutton view

--- src/components/atoms/LeaveButton.js
+++ src/components/atoms/LeaveButton.js
@@ -1,3 +1,23 @@
+import React from "react";
+import { Button } from "@mui/material";
+import styled from "styled-components";
+
+const StyledButton = styled(Button)`
+  background-color: brown;
+`;
+
 export const LeaveButton = () => {
-    return <></>
-}
+  //Todo: OnClick処理を追加
+  return <Btn>退席</Btn>;
+};
+const Btn = styled.a`
+  display: inline-block;
+  padding: 0.3em 3em;
+  text-decoration: none;
+  color: #ffffff;
+  border: solid 1px #000;
+  background-color: #5e2424;
+  border-radius: 3px;
+  transition: 0.4s;
+  cursor: pointer;
+`;
