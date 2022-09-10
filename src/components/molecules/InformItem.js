453884afec599f0a0ae15f0114b69e3657de05bd
MinamiInoue2323 <inoue@ics.keio.ac.jp>
2022-09-10 22:27:39 +0900

    [update] place items side by side

--- src/components/molecules/InformItem.js
+++ src/components/molecules/InformItem.js
@@ -1,3 +1,29 @@
+import { TunTunIcon } from "../atoms/TunTunIcon";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import Stack from "@mui/material/Stack";
+import styled from "styled-components";
+import { Typography } from "@mui/material";
+
 export const InformItem = () => {
-    return <></>
-}
+  return (
+    <>
+      <Stack direction={{ xs: "column", sm: "row" }}>
+        <Stack direction={{ xs: "column", sm: "row" }}>
+          <TunTunIcon />
+          <Typography variant="p">text</Typography>
+        </Stack>
+        <Stack direction={{ xs: "column", sm: "row" }}>
+          <NotifyCircle />
+          <ArrowForwardIosIcon />
+        </Stack>
+      </Stack>
+    </>
+  );
+};
+
+const NotifyCircle = styled.div`
+  height: 2em;
+  width: 2em;
+  background-color: #ff0000;
+  border-radius: 50%;
+`;
