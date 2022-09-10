10d5547ffe66a4c2aee806008a6a2e587a38fa0c
hirotask <hirotaskhashi@gmail.com>
2022-09-10 20:23:14 +0900

    [update]: DMのRouteを追加

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import {LecturePage} from "./components/pages/LecturePage";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import {DMPage} from "./components/pages/DMPage";
 
 
 export default function App() {
@@ -8,6 +9,7 @@ export default function App() {
         <Wrapper>
             <Router>
                 <Route exact path="/" component={LecturePage} />
+                <Route path="/dm" component={DMPage} />
             </Router>
         </Wrapper>
   );
