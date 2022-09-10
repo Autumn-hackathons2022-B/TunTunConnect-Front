952c8a5d1a2ff312dad980c735412f90c020c5c0
MinamiInoue2323 <inoue@ics.keio.ac.jp>
2022-09-10 21:59:25 +0900

    [update] remove never-used style

--- src/components/atoms/LeaveButton.js
+++ src/components/atoms/LeaveButton.js
@@ -2,10 +2,6 @@ import React from "react";
 import { Button } from "@mui/material";
 import styled from "styled-components";
 
-const StyledButton = styled(Button)`
-  background-color: brown;
-`;
-
 export const LeaveButton = () => {
   //Todo: OnClick処理を追加
   return <Btn>退席</Btn>;
