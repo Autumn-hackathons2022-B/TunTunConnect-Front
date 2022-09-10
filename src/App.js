5d21cb89ef9f4f0aa198c0aa699a2b85522c9399
hirotask <hirotaskhashi@gmail.com>
2022-09-10 20:17:24 +0900

    [update]: 起動したときにLecturePageが表示されるように

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import {LecturePage} from "./components/pages/LecturePage";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <Wrapper>
+          <LecturePage />
+        </Wrapper>
   );
 }
 
-export default App;
+const Wrapper = styled.div`
+  margin: 2em;
+`
--- src/components/pages/LecturePage.js
+++ src/components/pages/LecturePage.js
@@ -1,3 +1,3 @@
 export const LecturePage = () => {
-    return <></>
+    return <>LecturePageです</>
 }
