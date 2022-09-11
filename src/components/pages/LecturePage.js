3fab3e1bc6a28bd73d9d5ff04e8d928c376de60f
MinamiInoue2323 <inoue@ics.keio.ac.jp>
2022-09-11 10:47:59 +0900

    [update] align card 3 X 3

--- src/components/pages/LecturePage.js
+++ src/components/pages/LecturePage.js
@@ -1,7 +1,37 @@
-import {TunTunNotify} from "../molecules/TunTunNotify";
-import {UserCard} from "../molecules/UserCard";
-import {UserCardModel} from "../../model/UserCardModel";
+import { TunTunNotify } from "../molecules/TunTunNotify";
+import { UserCard } from "../molecules/UserCard";
+import { UserCardModel } from "../../model/UserCardModel";
+import Grid from "@mui/material/Grid";
+import styled from "styled-components";
 
 export const LecturePage = () => {
-    return <>LecturePageです<UserCard userCardModel={UserCardModel.Other}/></>
-}
+  const CardTypes = [
+    UserCardModel.TunTun,
+    UserCardModel.Other,
+    UserCardModel.Other,
+    UserCardModel.Other,
+    UserCardModel.MySelf,
+    UserCardModel.TunTun,
+    UserCardModel.Disabled,
+    UserCardModel.Disabled,
+    UserCardModel.Other,
+  ];
+  return (
+    <StyleCards>
+      <Grid container spacing={2}>
+        {CardTypes.map((Card, index) => {
+          return (
+            <Grid item xs={4} key={index}>
+              <UserCard userCardModel={Card} />
+            </Grid>
+          );
+        })}
+      </Grid>
+    </StyleCards>
+  );
+};
+
+const StyleCards = styled.div`
+  /* ユーザーカードコンポーネント */
+  margin: auto;
+`;
