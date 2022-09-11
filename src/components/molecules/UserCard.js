39a9c7564563e4c3f11a53b3e4f1033da785d9c7
MinamiInoue2323 <inoue@ics.keio.ac.jp>
2022-09-11 10:47:33 +0900

    [update] change card styles

--- src/components/molecules/UserCard.js
+++ src/components/molecules/UserCard.js
@@ -1,20 +1,35 @@
-import {TunTunIcon} from "../atoms/TunTunIcon";
+import { TunTunIcon } from "../atoms/TunTunIcon";
 import styled from "styled-components";
-import {UserCardModel} from "../../model/UserCardModel";
-import {MyIcon} from "../atoms/MyIcon";
+import { UserCardModel } from "../../model/UserCardModel";
+import { MyIcon } from "../atoms/MyIcon";
 
 export const UserCard = ({ userCardModel }) => {
-    switch (userCardModel) {
-        case UserCardModel.MySelf:
-            return <MySelf><MyIcon size={100}/></MySelf>
-        case UserCardModel.Disabled:
-            return <Disabled />
-        case UserCardModel.TunTun:
-            return <TunTun><TunTunIcon size={100}/>○○さんを<br />つんつんしました！</TunTun>
-        default:
-            return <Other><TunTunIcon size={100}/></Other>
-    }
-}
+  switch (userCardModel) {
+    case UserCardModel.MySelf:
+      return (
+        <MySelf>
+          <MyIcon size={100} />
+        </MySelf>
+      );
+    case UserCardModel.Disabled:
+      return <Disabled />;
+    case UserCardModel.TunTun:
+      return (
+        <TunTun>
+          <TunTunIcon size={100} />
+          ○○さんを
+          <br />
+          つんつんしました！
+        </TunTun>
+      );
+    default:
+      return (
+        <Other>
+          <TunTunIcon size={100} />
+        </Other>
+      );
+  }
+};
 
 const Other = styled.div`
   /* ユーザーカードコンポーネント */
@@ -25,14 +40,11 @@ const Other = styled.div`
   justify-content: center;
   align-items: center;
 
-  position: absolute;
   width: 399px;
   height: 271px;
-  left: 64px;
-  top: 70px;
 
-  background: #FFD18C;
-  border: 2px solid #AF7A2A;
+  background: #ffd18c;
+  border: 2px solid #af7a2a;
   border-radius: 18px;
 `;
 
@@ -44,18 +56,15 @@ const TunTun = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  
+
   font-size: 1.8em;
   color: #fff;
 
-  position: absolute;
   width: 399px;
   height: 271px;
-  left: 64px;
-  top: 70px;
 
-  background: #E88E81;
-  border: 2px solid #E35743;
+  background: #e88e81;
+  border: 2px solid #e35743;
   border-radius: 18px;
 `;
 
@@ -68,16 +77,13 @@ const Disabled = styled.div`
   justify-content: center;
   align-items: center;
 
-  position: absolute;
   width: 399px;
   height: 271px;
-  left: 64px;
-  top: 70px;
 
-  background: #D0D0D0;
+  background: #d0d0d0;
   border: 2px dotted #000000;
   border-radius: 18px;
-`
+`;
 
 const MySelf = styled.div`
   /* ユーザーカードコンポーネント */
@@ -88,13 +94,10 @@ const MySelf = styled.div`
   justify-content: center;
   align-items: center;
 
-  position: absolute;
   width: 399px;
   height: 271px;
-  left: 64px;
-  top: 70px;
 
-  background: #81AAE8;
+  background: #81aae8;
   border: 2px solid #202533;
   border-radius: 18px;
-`
+`;
