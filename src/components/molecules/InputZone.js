8bb22083cc7988efaf882c6d20b75b86978afea4
Hinaruhi <wavoquadcentvinsentislands@gmail.com>
2022-09-11 10:24:22 +0900

    Make InputZone which includes SubmitText and SubmitButton

---
+++ src/components/molecules/InputZone.js
@@ -0,0 +1,9 @@
+import { SubmitText } from "../atoms/SubmitText";
+import { SubmitButton } from "../atoms/SubmitButton";
+
+export const InputZone = () => {
+    return <div>
+        <SubmitText />
+        <SubmitButton />
+    </div>
+}
