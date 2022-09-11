7ed9c442606880218fefda8225463c651553e7e5
Hinaruhi <wavoquadcentvinsentislands@gmail.com>
2022-09-11 10:42:28 +0900

    make nowtext state

--- src/components/molecules/InputZone.js
+++ src/components/molecules/InputZone.js
@@ -1,7 +1,10 @@
 import { SubmitText } from "../atoms/SubmitText";
 import { SubmitButton } from "../atoms/SubmitButton";
+import { useState } from "react";
 
 export const InputZone = () => {
+    const [nowText, setNowtext] = useState("")
+    
     return <div>
         <SubmitText />
         <SubmitButton />
