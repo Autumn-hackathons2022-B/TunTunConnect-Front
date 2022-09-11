adb2bcb2e710d74898b725a71895da89a6f7d8fd
MinamiInoue2323 <inoue@ics.keio.ac.jp>
2022-09-11 11:19:13 +0900

    [update] implement leucure page

--- src/components/pages/LecturePage.js
+++ src/components/pages/LecturePage.js
@@ -3,6 +3,9 @@ import { UserCard } from "../molecules/UserCard";
 import { UserCardModel } from "../../model/UserCardModel";
 import Grid from "@mui/material/Grid";
 import styled from "styled-components";
+import { LeaveButton } from "../atoms/LeaveButton";
+import { MailButton } from "../atoms/MailButton";
+import schoolImage from "../../assets/img/school.jpg";
 
 export const LecturePage = () => {
   const CardTypes = [
@@ -17,21 +20,51 @@ export const LecturePage = () => {
     UserCardModel.Other,
   ];
   return (
-    <StyleCards>
-      <Grid container spacing={2}>
-        {CardTypes.map((Card, index) => {
-          return (
-            <Grid item xs={4} key={index}>
-              <UserCard userCardModel={Card} />
-            </Grid>
-          );
-        })}
-      </Grid>
-    </StyleCards>
+    <Wrapper>
+      <StyledLeaveButton>
+        <LeaveButton />
+      </StyledLeaveButton>
+
+      <StyleCards>
+        <Grid container spacing={2}>
+          {CardTypes.map((Card, index) => {
+            return (
+              <Grid item xs={4} key={index}>
+                <UserCard userCardModel={Card} />
+              </Grid>
+            );
+          })}
+        </Grid>
+      </StyleCards>
+      <StyledMailButton>
+        <MailButton isMidokuExist={true} />
+      </StyledMailButton>
+    </Wrapper>
   );
 };
 
 const StyleCards = styled.div`
   /* ユーザーカードコンポーネント */
-  margin: auto;
+  margin: 3em;
+`;
+
+const StyledLeaveButton = styled.div`
+  position: fixed;
+  top: 0;
+  right: 0;
+`;
+const StyledMailButton = styled.div`
+  position: fixed;
+  bottom: 0;
+  left: 0;
+`;
+const Wrapper = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-image: url(${schoolImage});
+  background-size: cover;
+  min-height: 100vh;
+  margin: 0;
+  padding: 0;
 `;
